0c9971746944853e076913f449ee8a2630714696
Mitchell <mstankovski@oakland.edu>
2021-11-04 18:51:01 -0400

    Reciveing

--- receiver.py
+++ receiver.py
@@ -1,27 +1,22 @@
 from helper import *
 
 def incoming_packets(pkt):
+    src = "35.50.12.218"
+    dst = get_if_addr(conf.iface)
     # filters incomming packets on scource and destination
     # Or is just for testing
-    try:
-        if ((pkt[IP].src == scr) or (pkt[IP].dst == dst) or (pkt[TCP].flags.S and pkt[TCP].flags.P)):
-            ## Example of what you see for summary
-            ## IP / TCP 64.4.54.254:https > 192.168.1.78:33374 A / Padding
-            # print(pkt[IP].summary())
-            ## Hex Packet Layout + Decoder
-            ## https://hpd.gasmi.net/
-            ## Notes for example Hex
-            # hexData = hexdump(pkt)
-            # print(bytes(pkt[TCP].payload))
-            encode_hash(pkt, get_last_8bit(pkt))
-    except:
-        print("NO")
+    if ((pkt[IP].src == src) or (pkt[IP].dst == dst)):
+        ## Example of what you see for summary
+        ## IP / TCP 64.4.54.254:https > 192.168.1.78:33374 A / Padding
+        print(pkt[IP].summary())
+        encode_hash(pkt, get_last_8bit(pkt))
+        return encode_hash(pkt, get_last_8bit(pkt))
 
 def receiver(args):
     src = args.srcip
     dst = get_if_addr(conf.iface)
-    sniff(filter="ip", prn=incoming_packets)
-    # print("Not implemented yet.")
+    capture = sniff(filter='ip and tcp', prn=incoming_packets, count = 5)
+    print(capture.show())
 
 """
 dst = get_if_addr(conf.iface)
